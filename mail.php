09b5d2897f85a6388296bc2f3b431a04c168465a
Dennis Müller <dennis.mueller@bib.uni-mannheim.de>
2020-06-03 14:03:59 +0200

    update mail logic to one of stefans states

--- mail.php
+++ mail.php
@@ -4,8 +4,34 @@ require_once 'Swift/swift_required.php';
 
 // Mail implementation using Swift Mailer.
 // See documentation: https://swiftmailer.symfony.com/docs/introduction.html
-function sendmail($uid, $email, $text) {
-    $text = "Diese Reservierungen sind für die Benutzerkennung $uid vorgemerkt:\n\n" . $text;
+function sendmail($uid, $text) {
+    // TODO: Add nice intro using name and sex.
+    // TODO: Optionally pass also pretty name.
+    global $ldap;
+
+    $to = $ldap['mailRoutingAddress'];
+    if ($to == '') {
+        $to = $ldap['mail'];
+    }
+    if ($to == '') {
+        alert("Keine E-Mail-Adresse für $uid gefunden");
+        return;
+    }
+
+    $text = "Diese Buchungen sind für die Benutzerkennung $uid vorgemerkt:\n\n$text";
+    // TODO: URL für Produktivbetrieb aktualisieren.
+    $text = "$text\nVerwalten Sie Ihre Buchungen hier: https://www.bib.uni-mannheim.de/reservation/?uid=$uid\n";
+    $text = "$text\nMit freundlichen Grüssen,\nIhre Universitaetsbibliothek";
+
+    // Anrede.
+    $sn = $ldap['sn'];
+    if ($ldap['rGender'] == 'M') {
+        $text = "Sie haben über unser Buchungssystem eine E-Mail-Benachrichtigung angefordert.\n\n$text";
+        $text = "Sehr geehrter Herr $sn,\n\n$text";
+    } elseif ($ldap['rGender'] == 'W') {
+        $text = "Sie haben über unser Buchungssystem eine E-Mail-Benachrichtigung angefordert.\n\n$text";
+        $text = "Sehr geehrte Frau $sn,\n\n$text";
+    }
 
     // Sendmail for transport.
     $transport = new Swift_SendmailTransport('/usr/sbin/sendmail -bs');
@@ -13,12 +39,15 @@ function sendmail($uid, $email, $text) {
     // Create the Mailer using your created Transport
     $mailer = new Swift_Mailer($transport);
 
-    $from = ['user@example.org' => 'Demo Sender'];
+    // TODO: Define from e-mail address.
+    $from = ['stefan.weil@bib.uni-mannheim.de' => 'Stefan Weil'];
 
     // Create a message
-    $message = (new Swift_Message('Sitzplatzreservierung'))
+    // TODO: Which subject?
+    $message = (new Swift_Message('UB Mannheim - Sitzplatzbuchungen'))
         ->setFrom($from)
-        ->setTo($email)
+        // TODO: to wahlweise auch mit Name: [mail => name].
+        ->setTo($to)
         ->setBody($text)
     ;
 
