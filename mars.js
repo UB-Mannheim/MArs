2443008df557a7c770e72bf88a8c24e809a24e36
BFallert <bernd.fallert@bib.uni-mannheim.de>
2020-09-25 14:50:01 +0200

    Test because Typ3 remove onclick

--- mars.js
+++ mars.js
@@ -7,3 +7,27 @@ function onlyOne(checkbox,cname) {
         };
     });
 };
+
+
+(function () {
+    function checkLikeRadio(tag) {
+        var form = document.getElementById(tag);//selecting the form ID
+        var checkboxList = form.getElementsByTagName("input");//selecting all checkbox of that form who will behave like radio button
+        for (var i = 0; i < checkboxList.length; i++) {//loop thorough every checkbox and set there value false.
+            if (checkboxList[i].type == "checkbox") {
+                checkboxList[i].checked = false;
+            }
+            checkboxList[i].onclick = function () {
+                checkLikeRadio(tag);//recursively calling the same function again to uncheck all checkbox
+                checkBoxName(this);// passing the location of selected checkbox to another function.
+            };
+        }
+    }
+
+    function checkBoxName(id) {
+        return id.checked = true;// selecting the selected checkbox and maiking its value true;
+    }
+    window.onload = function () {
+        checkLikeRadio("form");
+    };
+})();
